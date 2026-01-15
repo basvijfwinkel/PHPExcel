769c48e5e1f9edeb992800a284e5796cd16ed178
basvijfwinkel <bas5winkel@gmail.com>
2026-01-15 13:00:51 +0900

    Update Worksheet.php : fixed bug

--- Classes/PHPExcel/Writer/Excel2007/Worksheet.php
+++ Classes/PHPExcel/Writer/Excel2007/Worksheet.php
@@ -1014,7 +1014,7 @@ class PHPExcel_Writer_Excel2007_Worksheet extends PHPExcel_Writer_Excel2007_Writ
 			if (count($columns) > 0) {
 				foreach($columns as $columnID => $column) {
 					$rules = $column->getRules();
-					if (count($rules > 0)) {
+					if (count($rules) > 0) {
 						$objWriter->startElement('filterColumn');
 							$objWriter->writeAttribute('colId',	$pSheet->getAutoFilter()->getColumnOffset($columnID));
 
